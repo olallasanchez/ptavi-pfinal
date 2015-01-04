228e3197a2e7f09bc9898928f29b46df885a7245
Olalla <olalla_sanchez93@hotmail.com>
2015-01-04 19:17:00 +0100

    Terminado Log, falta PEP8 y detalles

--- uaserver.py
+++ uaserver.py
@@ -5,6 +5,7 @@ import sys
 import SocketServer
 import socket
 import os
+import uaclient
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
@@ -62,8 +63,8 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
     """
     Echo server class
     """
-    IP_RTP = '0000' 
-    Puerto_RTP = 0
+    RTP = [0,'0']
+
     def handle(self):
         while 1:
             # Leyendo línea a línea lo que nos envía el proxy/User Agent
@@ -72,56 +73,72 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
             if not line:
                 break
             print line
-            Linea = line.split('\r\n')
-            Linea = Linea[0].split(' ')
+            IP_Recib = self.client_address[0]
+            Puerto_Recib = self.client_address[1]
+            Linea0 = line.split('\r\n')
+            Linea = Linea0[0].split(' ')
             Metodo = Linea[0]
             Lista_Metodos = ["Invite", "Bye", "Ack"]
             UserName = Lista[0]['username']
-            if len(Linea) != 3 or Linea[2] != 'SIP/2.0':
-                Envio =  'SIP/2.0 400 Bad Request\r\n\r\n'
-                self.wfile.write(Envio)
-            else:
-                if not Metodo in Lista_Metodos:
-                    if line != '':
-                        Envio = 'SIP/2.0 405 Method Not Allowed\r\n\r\n'
-                        self.wfile.write(Envio)
+            uaclient.log(Lista[4]['path'],'Recibir',IP_Recib,Puerto_Recib,Linea0[0] + '\r\n')
+            try:
+                if len(Linea) != 3 or Linea[2] != 'SIP/2.0':
+                    Envio =  'SIP/2.0 400 Bad Request\r\n\r\n'
+                    self.wfile.write(Envio)
+                    Log =  'SIP/2.0 400 Bad Request\r\n'
+                    uaclient.log(Lista[4]['path'],'Enviar',IP_Recib,Puerto_Recib,Log)
                 else:
-                    if Metodo == 'Invite':
-                        Info = line.split('\r\n\r\n')
-                        SDP = Info[1].split('\r\n')
-                        print SDP
-                        IP = SDP[1].split(' ')
-                        self.IP_RTP = IP[3]
-                        Puerto = SDP[4].split(' ')
-                        self.Puerto_RTP = Puerto[3]
-                        Envio = 'SIP/2.0 100 Trying\r\n\r\n'
-                        Envio += 'SIP/2.0 180 Ringing\r\n\r\n'
-                        Envio += 'SIP/2.0 200 OK\r\n'
-                        Envio += 'Content-Type: application/sdp\r\n\r\n'
-                        Envio += 'v = 0\r\no = ' + UserName + ' ' + Lista[1]['ip']
-                        Envio += '\r\ns = misesion\r\n' + 't = 0\r\n' + 'm = audio '
-                        Envio += Lista[2]['puerto'] + ' RTP\r\n\r\n'
-                        Ip_vacia = str(Lista[1]['ip'])
-                        if not Ip_vacia:
-                            Ip_vacia = '127.0.0.1'
-                        print Envio
-                        self.wfile.write(Envio)
-                        print 'Enviado'
-                    elif Metodo == 'Ack':
-                        print 'RTP......'
-                        #aEjecutar es un string con lo que se ejecuta en la shell
-                        aEjecutar = './mp32rtp -i ' + self.IP_RTP + ' -p ' + str(self.Puerto_RTP) + '< '
-                        aEjecutar += Lista[5]['path']
-                        print "Vamos a ejecutar", aEjecutar
-                        os.system('chmod 755 mp32rtp')
-                        os.system(aEjecutar)
-                        print 'TERMINADO'
-                    elif Metodo == 'Bye':
-                        Envio = 'SIP/2.0 200 OK\r\n\r\n'
-                        print Envio
-                        self.wfile.write(Envio)
-
-
+                    if not Metodo in Lista_Metodos:
+                        if line != '':
+                            Envio = 'SIP/2.0 405 Method Not Allowed\r\n\r\n'
+                            self.wfile.write(Envio)
+                            Log = 'SIP/2.0 405 Method Not Allowed\r\n'
+                            uaclient.log(Lista[4]['path'],'Envio',IP_Recib,Puerto_Recib,Log)
+                    else:
+                        if Metodo == 'Invite':
+                            Info = line.split('\r\n\r\n')
+                            SDP = Info[1].split('\r\n')
+                            print SDP
+                            IP = SDP[1].split(' ')
+                            self.RTP[0] = IP[3]
+                            Puerto = SDP[4].split(' ')
+                            self.RTP[1] = Puerto[3]
+                            Envio = 'SIP/2.0 100 Trying\r\n\r\n'
+                            Envio += 'SIP/2.0 180 Ringing\r\n\r\n'
+                            Envio += 'SIP/2.0 200 OK\r\n'
+                            Envio += 'Content-Type: application/sdp\r\n\r\n'
+                            Envio += 'v = 0\r\no = ' + UserName + ' ' + Lista[1]['ip']
+                            Envio += '\r\ns = misesion\r\n' + 't = 0\r\n' + 'm = audio '
+                            Envio += Lista[2]['puerto'] + ' RTP\r\n\r\n'
+                            print Envio
+                            self.wfile.write(Envio)
+                            uaclient.log(Lista[4]['path'],'Enviar',IP_Recib,Puerto_Recib,'SIP/2.0 100 Trying\r\n')
+                            uaclient.log(Lista[4]['path'],'Enviar',IP_Recib,Puerto_Recib,'SIP/2.0 180 Ringing\r\n')
+                            uaclient.log(Lista[4]['path'],'Enviar',IP_Recib,Puerto_Recib,'SIP/2.0 200 OK\r\n')
+                            print 'Enviado'
+                        elif Metodo == 'Ack':
+                            print 'RTP......'
+                            #aEjecutar es un string con lo que se ejecuta en la shell
+                            aEjecutar = './mp32rtp -i ' + self.RTP[0] + ' -p ' + str(self.RTP[1]) + '< '
+                            aEjecutar += Lista[5]['path']
+                            print "Vamos a ejecutar", aEjecutar
+                            os.system('chmod 755 mp32rtp')
+                            os.system(aEjecutar)
+                            uaclient.log(Lista[4]['path'],'Enviar',self.RTP[0],self.RTP[1],'Envio RTP\r\n')
+                            print 'TERMINADO'
+                        elif Metodo == 'Bye':
+                            Envio = 'SIP/2.0 200 OK\r\n\r\n'
+                            print Envio
+                            self.wfile.write(Envio)
+                            Log = 'SIP/2.0 200 OK\r\n'
+                            uaclient.log(Lista[4]['path'],'Enviar',IP_Recib,Puerto_Recib,Log)
+                            uaclient.log(Lista[4]['path'],'Finish','','','')
+            except socket.error:
+                Texto = 'Error: No server listening at '
+                Texto += IP_Recib + ' port ' + str(Puerto_Recib) + '\r\n'
+                uaclient.log(Lista[4]['path'],'Error','','',Texto)
+                print Texto
+                raise SystemExit
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
@@ -133,8 +150,10 @@ if __name__ == "__main__":
             info_ua = sys.argv[1]
             parser.parse(open(info_ua))
             Lista = cHandler.get_tags()
-
+            if Lista[1]['ip'] == '':
+                Lista[1]['ip'] = '127.0.0.1'
             serv = SocketServer.UDPServer(("", int(Lista[1]['puerto'])), EchoHandler)
+            uaclient.log(Lista[4]['path'],'Start','','','')
             print "Listening..."
             serv.serve_forever()
         else:
